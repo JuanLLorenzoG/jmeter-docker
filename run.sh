7c9f9f03247ea054cb1b60dbf094f75702fac9e8
fran.aznar <francisco.aznar@nexoqa.com>
2022-10-28 11:51:11 +0200

    add jmeter

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ NAME="jmeter"
 IMAGE="jmeter/docker:5.2.1"
 
 # Finally run
-sudo docker run --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
+docker run --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
