186702500ca014a806b71729c7dd6724da9f1606
fran.aznar <francisco.aznar@nexoqa.com>
2022-10-31 09:51:26 +0100

    add perform test pipeline

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,9 +13,9 @@
 # n=$(($freeMem/10*2))
 # export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
 
-# echo "                                      "
-# echo "                                      "
-# echo "----  STARTING JMeter Test ---- `date`"
+echo "                                      "
+echo "                                      "
+echo "----  STARTING JMeter Test ---- `date`"
 # echo "JVM_ARGS=${JVM_ARGS}"
 # echo "jmeter args=$@"
 
