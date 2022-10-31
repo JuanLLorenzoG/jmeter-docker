4b8c455dc4fefc4306db2fa13ca1e9703ce9dba2
fran.aznar <francisco.aznar@nexoqa.com>
2022-10-31 09:22:42 +0100

    add test

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 # Run JMeter Docker image with options
 
 NAME="jmeter"
-IMAGE="jmeter/docker:5.2.1"
+IMAGE="jmeter/docker:5.5"
 
 # Finally run
 docker stop ${NAME} > /dev/null 2>&1
 docker rm ${NAME} > /dev/null 2>&1
 # docker run --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
-docker run --name ${NAME} -i ${IMAGE} $@
+docker run --name ${NAME} -i -v ${PWD}:/home/jmeter/apache-jmeter-5.5/ ${IMAGE} $@
