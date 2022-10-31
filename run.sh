dcdd3246c3857c487ef7f95e3b4c4bd69234f264
fran.aznar <francisco.aznar@nexoqa.com>
2022-10-31 09:44:32 +0100

    add perform test pipeline

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ IMAGE="jmeter/docker:5.5"
 docker stop ${NAME} > /dev/null 2>&1
 docker rm ${NAME} > /dev/null 2>&1
 # docker run --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
-docker run --name ${NAME} -i -v ${PWD}/test:/home/jmeter/apache-jmeter-5.5/test ${IMAGE} $@
+docker run --name ${NAME} -i -v /home/jenkins/workspace/_app_perform-test-implementation/jmeter-docker/test:/home/jmeter/apache-jmeter-5.5/test ${IMAGE} $@
