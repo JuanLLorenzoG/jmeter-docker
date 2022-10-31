793ecbe108ade970b9c1cbccd5cf334aeceaddf4
fran.aznar <francisco.aznar@nexoqa.com>
2022-10-31 10:05:33 +0100

    add perform test pipeline

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,21 +6,21 @@
 # This script expects the standdard JMeter command parameters.
 #
 
-# set -e
-# freeMem=`awk '/MemFree/ { print int($2/1024) }' /proc/meminfo`
-# s=$(($freeMem/10*8))
-# x=$(($freeMem/10*8))
-# n=$(($freeMem/10*2))
-# export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
+set -e
+freeMem=`awk '/MemFree/ { print int($2/1024) }' /proc/meminfo`
+s=$(($freeMem/10*8))
+x=$(($freeMem/10*8))
+n=$(($freeMem/10*2))
+export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
 
 echo "                                      "
 echo "                                      "
 echo "----  STARTING JMeter Test ---- `date`"
-# echo "JVM_ARGS=${JVM_ARGS}"
-# echo "jmeter args=$@"
+echo "JVM_ARGS=${JVM_ARGS}"
+echo "jmeter args=$@"
 
-# jmeter $@
+jmeter $@
 
-# echo "----  ENDING JMeter Test ---- `date`"
-# echo "                                      "
-# echo "                                      "
+echo "----  ENDING JMeter Test ---- `date`"
+echo "                                      "
+echo "                                      "
